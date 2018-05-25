6b89336a1850c4d96e52626953eaa60b89c6c90b
zjs <henu_zjs@163.com>
2018-05-25 15:51:19 +0800

    一个简单的Maven程序

---
+++ hello-world/src/main/java/com/mycode/hello_world/App.java
@@ -0,0 +1,13 @@
+package com.mycode.hello_world;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
